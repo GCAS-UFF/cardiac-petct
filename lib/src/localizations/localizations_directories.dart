b54008945fa7597dadf0373ba76d81234df14dd5
Giovane Lopes <giovanelopes>
2023-04-14 14:53:02 -0300

    chore: translate email verify strings

--- lib/src/localizations/localizations_directories.dart
+++ lib/src/localizations/localizations_directories.dart
@@ -3,6 +3,7 @@ class LocalizationsDirectories {
     'lib/features/auth/i18n/',
     'lib/features/auth/submodules/login/i18n/',
     'lib/features/auth/submodules/registration/i18n/',
+    'lib/features/auth/submodules/email_verify/i18n/',
     'lib/features/auth/domain/erros/i18n/',
   ];
 }
